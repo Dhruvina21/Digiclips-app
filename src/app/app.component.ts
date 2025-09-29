40f7a9fc7556f795e720e41a8a56971a14a6f040
Pavel Storch <pstorch@asu.edu>
2025-09-29 16:41:34 -0700

    Implement navigation skeleton with Main Menu and Submenu routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './components/team-member-2/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/submenu/submenu.component.ts
+++ src/app/pages/submenu/submenu.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-submenu',
-  imports: [],
+  standalone: true,
   templateUrl: './submenu.component.html',
   styleUrl: './submenu.component.css'
 })
-export class SubmenuComponent {
-
-}
+export class SubmenuComponent {}
