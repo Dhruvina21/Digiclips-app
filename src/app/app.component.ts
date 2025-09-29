0ad139cd4676c0d83ed872569708a6c05b82d77e
tyw03 <tatracy@asu.edu>
2025-09-29 00:35:41 -0700

    Integrated the search form into the main app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchFormComponent } from "./components/team-member-3/search-form/search-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
